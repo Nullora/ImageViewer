beee64ac0d0024cb2ea4db8a1c3eb38153bada75
Nullora <nullora@tutamail.com>
2026-03-31 21:11:03 +0100

    test

---
+++ README.md
@@ -0,0 +1,6 @@
+# Image Viewer
+Made purely for fun and to learn more about C. fully inspired from [Daniel Hirsch](https://www.youtube.com/watch?v=sItRLFjbqvo) He's way better than me at this!
+# Why I made it
+I was bored on a casual Tuesday and came across a video from Daniel and decided to follow his footsteps.
+# Installation and usage
+Don't actually use this seriously I made it to learn sdl2 not as a reliable replacement!
--- iv.c
+++ iv.c
@@ -3,25 +3,51 @@
 #include<SDL2/SDL.h>
 
 int main(){
-    //open window
-    SDL_Window *pwindow  = SDL_CreateWindow("hello!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, 0); 
+        //header data
+    FILE *in = stdin;
+    char *pthrow = calloc(1000, sizeof(char));
+    //read first line (header [P6 for binary]) {skip}
+    fgets(pthrow, 1000, in);
+    //read second line (comment) {skip}
+    fgets(pthrow, 1000, in);
+    //read third line (width height) {important}
+    char *pdimensions = calloc(1000, sizeof(char));
+    fgets(pdimensions, 1000, in);
+    int width=-1;
+    int height=-1;
+    //best function in the world easiest parse of my life
+    sscanf(pdimensions, "%d %d\n", &width, &height);
+    free(pdimensions);
+    //debug
+    printf("width=%d height=%d\n", width, height);
+    //read 4th line (max color value) {skip}
+    fgets(pthrow, 1000, in);
+    free(pthrow);
+
+
+
+    SDL_Window *pwindow  = SDL_CreateWindow("hello!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0); 
     //get window surface to draw on
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
-    //rgb values
-    Uint8 r,g,b;
-    r = 0xFF;
-    g = 0x0;
-    b = 0x0;
-    Uint32 color = SDL_MapRGB(psurface->format, r,g,b);
+
     //specific pixel with SDL_Rect {x,y,1,1}
-    int x = 50;
-    int y = 50;
-    const SDL_Rect pixel = (SDL_Rect){x,y,1,1};
-    //fill that pixel.
-    //fillrect takes (surface, rectangle address (or NULL if you wanna fill the whole screen), color (from SDL_MapRGB))
-    SDL_FillRect(psurface, &pixel, color);
-    //update screen
+    SDL_Rect pixel = (SDL_Rect){0,0,1,1};
+    Uint32 color = 0x00;
+    for(int y = 0; y<width; y++){
+        for(int x = 0; x<height;x++){
+            //rgb values
+            Uint8 r,g,b;
+            //grab from file
+            r = (char)getchar();
+            g = (char)getchar();
+            b = (char)getchar();
+            color = SDL_MapRGB(psurface->format, r,g,b);
+            pixel.x = x;
+            pixel.y = y;
+            //fillrect takes (surface, rectangle address (or NULL if you wanna fill the whole screen), color (from SDL_MapRGB))
+            SDL_FillRect(psurface, &pixel, color);
+        }
+    }
     SDL_UpdateWindowSurface(pwindow);
-    //delay to see window
     SDL_Delay(3000);
 }
